26cd81a5030cdbb7b0a91eda9d488974c7ef8372
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-07 14:02:42 +0300

    map

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''A type annotated sum_list function'''
+import typing
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     '''A type annotated sum_list function'''
     return sum(map(float, input_list))
