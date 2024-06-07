e7c040c448abaf26820252490974035829fa4003
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-07 13:45:42 +0300

    To string

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A type annotated floor function'''
+
+
+def floor(n: float) -> int:
+    '''A type annotated floor function'''
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A type annotated to_str function'''
+
+
+def to_str(n: float) -> str:
+    '''A type annotated to_str function'''
+    return str(n)
